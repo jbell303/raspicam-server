10be2cf400d2f90d25b00e339416f7ea7ba15b85
James Bell <jbell303@James-Bells-iMac.local>
2018-03-20 19:54:12 -0700

    put raspicam function on backend

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 var path = require('path');
+var RaspiCam = require('raspicam');
+var photo_path = "~/Desktop/photo.jpg";
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/photo', function (req, res) {
+	var camera = new RaspiCam({mode: "photo", output: photo_path});
+	console.log("taking picture");
+	res.send("taking picture");
+	camera.start();
+})
 
 app.listen(port, () => console.log('Server started! At http://localhost:' + port));
--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const button = document.querySelector('#button1');
 const responseContainer = document.querySelector('#response-container');
-var RaspiCam = require("raspicam");
 
 button.addEventListener('click', function () {
-	var camera = new RaspiCam({mode:photo, output: "~/Desktop/photo.jpg"});
-	camera.start();
-	console.log("taking picture"); 
+	responseContainer.innerHTML = 'Button clicked!';
 });
