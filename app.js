664c82f2b478c71fe03b28063680ddf7fda81eb3
James Bell <jbell303@James-Bells-iMac.local>
2018-03-21 11:16:57 -0700

    change filename

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 var path = require('path');
 var RaspiCam = require('raspicam');
 var d = new Date();
-var dateTime = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + "-" + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(); 
-var photo_path = `/home/pi/Desktop/${dateTime}.jpg`;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/photo', function (req, res) {
+	var dateTime = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + "-" + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(); 
+	var photo_path = `/home/pi/Desktop/${dateTime}.jpg`;
 	var camera = new RaspiCam({mode: "photo", output: photo_path});
 	console.log("taking picture");
 	res.send("taking picture");
