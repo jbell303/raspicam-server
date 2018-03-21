eb8a1dc372635d0e6408d67a311bf8b8fa6f6dd9
James Bell <jbell303@gmail.com>
2018-03-20 19:40:51 -0700

    add raspicam

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('#button1');
 const responseContainer = document.querySelector('#response-container');
+var RaspiCam = require("raspicam");
 
 button.addEventListener('click', function () {
-	responseContainer.innerHTML = 'Hello Julie!';
+	var camera = new RaspiCam({mode:photo, output: "~/Desktop/photo.jpg"});
+	camera.start();
+	console.log("taking picture"); 
 });
