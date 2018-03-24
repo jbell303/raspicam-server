6955191e40314b9408690cafe01f854c82aab810
James Bell <jbell303@gmail.com>
2018-03-23 20:24:02 -0700

    fix filename

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 var path = require('path');
 var RaspiCam = require('raspicam');
-var d = new Date();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/photo', function (req, res) {
+	var d = new Date();
 	var dateTime = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + "-" + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(); 
 	var photo_path = `/home/pi/Desktop/${dateTime}.jpg`;
 	var camera = new RaspiCam({mode: "photo", output: photo_path});
